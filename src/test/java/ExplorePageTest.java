39b5c5fc176aaba555a6fa9be31eb9872c55d9fb
Hitesh0304 <31934805+Hitesh0304@users.noreply.github.com>
2025-03-10 21:21:20 -0400

    added setup method for ExplorePageTest.java

--- src/test/java/ExplorePageTest.java
+++ src/test/java/ExplorePageTest.java
@@ -1,30 +1,32 @@
 import hiteshtestautomation.pages.ExplorePage;
 import hiteshtestautomation.pages.OnboardingPage;
 import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class ExplorePageTest extends BaseTest {
+    ExplorePage explorePage;
+
+    @BeforeClass
+    public void setup() {
+        explorePage = onboardingPage.clickSkipButton();
+        explorePage.verifyOnExplorePage();
+    }
 
     @Test
     public void featureArticleSectionLoadsTest() {
-        ExplorePage explorePage = onboardingPage.clickSkipButton();
-        explorePage.verifyOnExplorePage();
         Assert.assertEquals(explorePage.getFeaturedArticleHeader().getText(), "Featured article");
     }
 
     @Test
     public void pullToRefreshTest() {
-        ExplorePage explorePage = onboardingPage.clickSkipButton();
-        explorePage.verifyOnExplorePage();
         explorePage.pullToRefresh();
         Assert.assertTrue(explorePage.getFeaturedArticleHeader().isDisplayed());
     }
 
     @Test
     public void searchDisplayedTest() {
-        ExplorePage explorePage = onboardingPage.clickSkipButton();
-        explorePage.verifyOnExplorePage();
         Assert.assertTrue(explorePage.isSearchContainerDisplayed());
     }
 }
